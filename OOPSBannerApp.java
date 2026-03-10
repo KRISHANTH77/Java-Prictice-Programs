e3c517e6e07278c71404c82f5eb7acdce917c6bc
KRISHANTH77 <krishanthvamsi07@gmail.com>
2026-03-10 21:24:52 +0530

    UC5 - Array initialization using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,43 +1,18 @@
 public class OOPSBannerApp {
 
-    // Method to print letter O
-    public static void printO() {
-        System.out.println(" *** ");
-        System.out.println("*     *");
-        System.out.println("*     *");
-        System.out.println("*     *");
-        System.out.println(" *** ");
-    }
-
-    // Method to print letter P
-    public static void printP() {
-        System.out.println("*** ");
-        System.out.println("*    *");
-        System.out.println("*** ");
-        System.out.println("*     ");
-        System.out.println("*     ");
-    }
-
-    // Method to print letter S
-    public static void printS() {
-        System.out.println(" ***");
-        System.out.println("*     ");
-        System.out.println(" ***");
-        System.out.println("      *");
-        System.out.println(" ***");
-    }
-
     public static void main(String[] args) {
 
-        printO();
-        System.out.println();
+        String[] banner = {
+            String.join("", " **   ", "**   ", "**   ", "** "),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *"),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *"),
+            String.join("", "*     *  ", "**    ", "**    ", "*** "),
+            String.join("", "*     *  ", "*        ", "*        ", "*   "),
+            String.join("", " **   ", "        ", "**    ", "   ")
+        };
 
-        printO();
-        System.out.println();
-
-        printP();
-        System.out.println();
-
-        printS();
+        for(String line : banner) {
+            System.out.println(line);
+        }
     }
 }
