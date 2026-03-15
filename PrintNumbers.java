42525964f33a7bc5a4bd7f14780be0691ee3d799
KRISHANTH77 <krishanthvamsi07@gmail.com>
2026-03-15 13:59:12 +0530

    Added Printumbers for loop management

---
+++ PrintNumbers.java
@@ -0,0 +1,17 @@
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        // Declare variables
+        int start = 1;
+        int end = 10;
+
+        // For loop to print numbers
+        for (int i = start; i <= end; i++) {
+
+            System.out.println(i);
+
+        }
+
+    }
+}
